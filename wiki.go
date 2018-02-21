f049b8c864b888ea559b69cf7e1934a45a76ef95
stefan safranek <stefan@stefan.stefan>
2018-02-20 20:55:08 -0800

    static file handler for images, username and passwords

--- wiki.go
+++ wiki.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/russross/blackfriday"
+
+	"./utils"
 )
 
 const (
@@ -23,6 +28,7 @@ var (
 	HTML_TEMPLATE_FILE string = DEFAULT_HTML_TEMPLATE_FILE_FILE
 	HTML_TEMPLATE_NAME string = ""
 	TEMPLATES          *template.Template
+	USERS              Users
 )
 
 type Page struct {
@@ -30,12 +36,19 @@ type Page struct {
 	Body  template.HTML
 }
 
-func getFilename(page string) string {
+type WikiEngine struct{}
+
+func (self *WikiEngine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	self.viewHandler(w, r)
+}
+
+func (self *WikiEngine) getFilename(page string) string {
 	return fmt.Sprintf("%v/%v.md", CONTENT_DIRECTORY, page)
 }
 
-func loadPage(page string) (*Page, error) {
-	filename := getFilename(page)
+func (self *WikiEngine) loadPage(page string) (*Page, error) {
+	filename := self.getFilename(page)
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -43,25 +56,102 @@ func loadPage(page string) (*Page, error) {
 	return &Page{Title: page, Body: template.HTML(blackfriday.MarkdownCommon([]byte(body)))}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func (self *WikiEngine) viewHandler(w http.ResponseWriter, r *http.Request) {
 	page := r.URL.Path[1:]
 	if len(page) == 0 {
-		http.Redirect(w, r, "/Index", 301)
+		err := errors.New("Page not found")
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	p, err := loadPage(page)
+	p, err := self.loadPage(page)
 	if err != nil {
-		http.Redirect(w, r, "/", 301)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	renderTemplate(w, HTML_TEMPLATE_NAME, p)
+	self.renderTemplate(w, HTML_TEMPLATE_NAME, p)
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func (self *WikiEngine) renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := TEMPLATES.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (self *User) SetPassword(password string) {
+	self.Password = utils.SHA512FromBytes([]byte(password))
+}
+
+func (self *User) IsPassword(password string) bool {
+	return self.Password == utils.SHA512FromBytes([]byte(password))
+}
+
+type Users struct {
+	Users []*User `json:"users"`
+}
+
+// Fetch: fetches json file containing db4iot datasource schema.
+// @args file{string}	schema file
+func (self *Users) Fetch(file string) error {
+	b, err := ioutil.ReadFile(file)
+	if nil != err {
+		return err
+	}
+	return self.Unmarshal(string(b))
+}
+
+// Save: saves schema to json file
+func (self *Users) Save(file string) error {
+	contents, err := self.Marshal()
+	if nil != err {
+		return err
+	}
+	return ioutil.WriteFile(file, []byte(contents), 0644)
+}
+
+// Unmarshal: json unmarshals string to struct
+// @args string
+// @return error
+func (self *Users) Unmarshal(data string) error {
+	return json.Unmarshal([]byte(data), self)
+}
+
+// Marshal: json marshals struct
+// @return string
+// @return error
+func (self Users) Marshal() (string, error) {
+	b, err := json.Marshal(self)
+	if nil != err {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (self *Users) Get(username string) (*User, error) {
+	for _, user := range self.Users {
+		if username == user.Username {
+			return user, nil
+		}
+	}
+	return &User{}, errors.New("User not found")
+}
+
+func (self *Users) Has(username string) bool {
+	_, err := self.Get(username)
+	return err == nil
+}
+
+func (self *Users) Add(user *User) error {
+	if !self.Has(user.Username) {
+		self.Users = append(self.Users, user)
+		return nil
+	}
+	return errors.New("User already exists")
 }
 
 func init() {
@@ -72,12 +162,30 @@ func init() {
 
 	TEMPLATES = template.Must(template.ParseFiles(HTML_TEMPLATE_FILE))
 	HTML_TEMPLATE_NAME = strings.Replace(HTML_TEMPLATE_FILE, ".html", "", -1)
+
+	// set wiki users
+	USERS = Users{}
+	USERS.Fetch("users.json")
+	user := User{Username: "admin"}
+	USERS.Add(&user)
+	user.SetPassword("dev")
+	USERS.Save("users.json")
+
 }
 
 func main() {
-	http.HandleFunc("/", viewHandler)
+	// http://www.alexedwards.net/blog/a-recap-of-request-handling
+	wiki := &WikiEngine{}
+	// http.Handle("/wiki/", http.StripPrefix("/wiki", wiki))
+	http.Handle("/", wiki)
+
+	// Static Files
+	fs := http.FileServer(http.Dir("img"))
+	http.Handle("/img/", http.StripPrefix("/img/", fs))
+
 	fmt.Printf("Magic happens on port %v...\n", PORT)
 	err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+	// err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), mux)
 	if nil != err {
 		panic(err)
 	}
